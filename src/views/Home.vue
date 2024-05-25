4112b6fe24e07697b250bc93b64b49ae0f8d8f64
André Albson <andre.albson@precode.com.br>
2024-05-25 05:41:10 -0300

    Add Home view with CityList component
    Implemented a new Home view into the application and added the CityList component to it. The CityList component will be used to display a list of cities on the Home view. This change enhances the information shown on the home page and improves the user experience.

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <CityList />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CityList from "@/components/CityList.vue";
+
+export default Vue.extend({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  components: {
+    CityList,
+  },
+});
+</script>
