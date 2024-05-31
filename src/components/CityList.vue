44f4a94a48a6a9256cf922319fa03b0226ac6b60
André Albson <andre.albson@precode.com.br>
2024-05-31 12:34:49 -0300

    Add NavBar component and modify CityList layout
    Updated the CityList.vue layout by integrating the navigation bar, enhancing the application's UI. A new NavBar.vue component was created to provide application-wide navigation. Other updates include revised instruction text for the city addition operation and tweaks to the CSS code.

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -1,40 +1,52 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" sm="6" md="4">
-        <v-text-field
-          v-model="newCity"
-          label="City"
-          @keyup.enter="addCity"
-        ></v-text-field>
-        <v-btn @click="addCity">Add New City</v-btn>
-      </v-col>
-      <v-col cols="12" sm="12" md="12">
-        <v-divider></v-divider>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        cols="12"
-        sm="6"
-        md="3"
-        xl="2"
-        v-for="city in cities"
-        :key="city.name"
-      >
-        <CityWeather
-          :city="city"
-          @remove="removeCity"
-          @details="showDetails"
-        ></CityWeather>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app class="app-main">
+    <NavBar></NavBar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <h1 class="display-1">Real-time weather information</h1>
+          <p class="text--secondary">
+            To add a new city to the list, type the name of the city and click the <strong>"Add new City"</strong> button or <strong>press the "Enter" key</strong>.
+          </p>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field
+              v-model="newCity"
+              label="City Name"
+              @keyup.enter="addCity"
+            ></v-text-field>
+            <v-btn @click="addCity">Add New City</v-btn>
+          </v-col>
+          <v-col cols="12" sm="12" md="12">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="3"
+            xl="2"
+            v-for="city in cities"
+            :key="city.name"
+          >
+            <CityWeather
+              :city="city"
+              @remove="removeCity"
+              @details="showDetails"
+            ></CityWeather>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import CityWeather from "./CityWeather.vue";
+import NavBar from "@/components/NavBar.vue";
 
 interface City {
   name: string;
@@ -42,9 +54,14 @@ interface City {
 
 export default Vue.extend({
   name: "CityList",
-  components: { CityWeather },
+  components: { NavBar, CityWeather },
   data() {
     return {
+      app: {
+        title: "Weather App",
+        linkHome: "Home",
+        linkAbout: "About",
+      },
       cities: [
         { name: "Porto Alegre" },
         { name: "Rio de Janeiro"},
@@ -77,4 +94,3 @@ export default Vue.extend({
   }
 });
 </script>
-
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app>
+      <v-toolbar flat class="toolbar">
+        <v-toolbar-title>{{app.title}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text>{{app.linkAbout}}</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NavBar",
+  data() {
+    return {
+      app: {
+        title: "Weather App",
+        linkHome: "Home",
+        linkAbout: "About",
+      },
+    };
+  },
+});
+</script>
